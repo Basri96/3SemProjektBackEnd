bd3ffe971875ffc093dc9640dbf82b3a2cfd435f
felex <felicxmose@msn.com>
2019-05-08 21:51:54 +0200

    added model classes

---
+++ RestSmartTrashService/RestSmartTrashService/Model/recipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestSmartTrashService.Model
+{
+    public class recipe
+    {
+        int id { get; set; }
+        DateTime date { get; set; }
+        string recipeDescription { get; set; }
+        string recipeTitle { get; set; }
+        
+        public recipe(int id, DateTime date, string recipeDescription, string recipeTitle)
+        {
+            this.id = id;
+            this.date = date;
+            this.recipeDescription = recipeDescription;
+            this.recipeTitle = recipeTitle;
+        }
+
+        public recipe() { }
+    }
+}
---
+++ RestSmartTrashService/RestSmartTrashService/Model/weightMeassure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestSmartTrashService.Model
+{
+    public class weightMeassure
+    {
+        DateTime date { get; set; }
+        float weight { get; set; }
+
+        public weightMeassure(DateTime date, float weight)
+        {
+            this.date = date;
+            this.weight = weight;
+        }
+        public weightMeassure() { }
+    }
+}
